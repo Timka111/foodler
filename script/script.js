8777092d20cceccd7fce9e74d9ca216b05acf868
Timofey <110742885+Timka111@users.noreply.github.com>
2023-08-08 19:55:46 +0300

    feat: create new functiom

--- script/script.js
+++ script/script.js
@@ -18,3 +18,35 @@ const swiper = new Swiper(".swiper", {
     delay: 4000,
   },
 });
+
+const mainItems = document.querySelectorAll(".main__item");
+const mainBtn = document.querySelector(".main__btn");
+
+for (let item of mainItems) {
+  item.addEventListener("click", function () {
+    const title = document.querySelector("#" + item.dataset.goto);
+    const gotoBlockValue = title.getBoundingClientRect().top + scrollY;
+
+    window.scrollTo({
+      top: gotoBlockValue,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+}
+
+document.addEventListener("scroll", function () {
+  if (scrollY >= 100) {
+    mainBtn.classList.remove("hidden");
+  } else {
+    mainBtn.classList.add("hidden");
+  }
+});
+
+mainBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
